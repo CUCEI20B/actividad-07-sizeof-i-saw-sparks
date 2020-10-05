25c8bb08542ed62ac761233b197b76a0a446b484
i-saw-sparks <andresmolinaceti@gmail.com>
2020-10-05 11:20:03 -0500

    sizeof implemented

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
 
+int main() {
+    string in;
+
+    cin>>in;
+
+    if(in == "char")
+    cout<<sizeof(char);
+    else if (in == "int")
+    cout<<sizeof(int);
+    else if (in == "short")
+    cout<<sizeof(short);
+    else if (in == "float")
+    cout<<sizeof(float);
+    else if (in == "double")
+    cout<<sizeof(double);
+    else if (in == "long")
+    cout<<sizeof(long);
+    
     return 0;
 }
